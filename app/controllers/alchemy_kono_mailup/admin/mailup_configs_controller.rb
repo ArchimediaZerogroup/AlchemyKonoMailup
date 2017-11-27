528c180ff000594f32f99efdd456873ab9dcff2f
Marino Bonetti <marinobonetti@gmail.com>
2017-11-27 11:12:35 +0100

    show lista newsletters

--- app/controllers/alchemy_kono_mailup/admin/mailup_configs_controller.rb
+++ app/controllers/alchemy_kono_mailup/admin/mailup_configs_controller.rb
@@ -4,6 +4,11 @@ module AlchemyKonoMailup::Admin
 
     def main
       authorize! :main, :alchemy_kono_mailup_admin_mailup_configs
+
+      if KonoMailup::API.client_ready?
+        @client = KonoMailup::API.new
+      end
+
     end
 
   end
